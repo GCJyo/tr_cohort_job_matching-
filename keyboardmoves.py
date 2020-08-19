d5872e57beadaa7dfbf8e47a0d276395db762869
Jyothsna <59825199+GCJyo@users.noreply.github.com>
2020-08-19 16:44:43 +0100

    Add files via upload
    paring exercise code

---
+++ keyboardmoves.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# TV remote number of steps
+myKeyboard = [["" for r in range(8)] for f in range(5)]
+myKeyboard[0][0]="a"
+myKeyboard[0][1]="b"
+myKeyboard[0][2]="c"
+myKeyboard[0][3]="d"
+myKeyboard[0][4]="e"
+myKeyboard[0][5]="1"
+myKeyboard[0][6]="2"
+myKeyboard[0][7]="3"
+myKeyboard[1][0]="f"
+myKeyboard[1][1]="g"
+myKeyboard[1][2]="h"
+myKeyboard[1][3]="i"
+myKeyboard[1][4]="j"
+myKeyboard[1][5]="4"
+myKeyboard[1][6]="5"
+myKeyboard[1][7]="6"
+myKeyboard[2][0]="k"
+myKeyboard[2][1]="l"
+myKeyboard[2][2]="m"
+myKeyboard[2][3]="n"
+myKeyboard[2][4]="o"
+myKeyboard[2][5]="7"
+myKeyboard[2][6]="8"
+myKeyboard[2][7]="9"
+myKeyboard[3][0]="p"
+myKeyboard[3][1]="q"
+myKeyboard[3][2]="r"
+myKeyboard[3][3]="s"
+myKeyboard[3][4]="t"
+myKeyboard[3][5]="."
+myKeyboard[3][6]="@"
+myKeyboard[3][7]="0"
+myKeyboard[4][0]="u"
+myKeyboard[4][1]="v"
+myKeyboard[4][2]="w"
+myKeyboard[4][3]="x"
+myKeyboard[4][4]="y"
+myKeyboard[4][5]="z"
+myKeyboard[4][6]="_"
+myKeyboard[4][7]="/"
+#word=input("Enter the word: ")
+#word = "code"
+def split(splWord):
+    lstword=[]
+    for i in splWord:
+        lstword.append(i)
+    return lstword
+print(split("code"))
+#print(myKeyboard)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
